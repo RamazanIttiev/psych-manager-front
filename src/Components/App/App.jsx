33fc9cfe1f4e230e7f57e187e25ff5f4a94d766a
Ramazan <ramazzan.ittiev@gmail.com>
2020-11-05 23:17:45 +0300

    Added Header no icons

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App';
+import Header from '../Header/Header.jsx'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
