2ae34b749805aa46b2abdf7faed7e1088cc8b307
IvanKirsanov <darkhunt3r74@gmail.com>
2020-11-12 01:04:12 +0500

    Сессии

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SideBar = () => {
+  return <aside className="main-sidebar sidebar-dark-primary elevation-4"></aside>;
+};
+
+export default SideBar;
---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'admin-lte/dist/css/adminlte.min.css';
+import 'jqvmap-novulnerability/dist/jqvmap.min.css';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
+import 'admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css';
+//import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import 'admin-lte/plugins/datatables/jquery.dataTables.min.js';
+import 'datatables.net/js/jquery.dataTables.min.js';
+
+import 'admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js';
+import 'datatables.net-bs4/js/dataTables.bootstrap4.min.js';
+
+import 'datatables.net-responsive/js/dataTables.responsive.min.js';
+import 'admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min.js';
+
+import 'datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js';
+import 'admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js';
+
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+//import 'admin-lte/dist/js/adminlte.min.js';
+//import 'admin-lte/docs_html/assets/js/adminlte.min.js';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
