b1b29659b79a8013cdc4127e5b777df2d6d8ebdb
Ramazan <ramazzan.ittiev@gmail.com>
2020-11-12 15:41:47 +0300

    fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import Header from '../Header/Header';
-import Session from '../Session/Session';
-import SessionTable from '../SessionTable/SessionTable';
-import SideBar from '../SideBar/SideBar';
+import './App';
+import Header from '../Header/Header.jsx'
 
-const App = () => {
+function App() {
   return (
-    <div className="wrapper skin-blue layout-top-nav">
-      <Header />
-      <SideBar />
-      <div className="content-wrapper"  style={{marginTop: '30px'}}>
-        <section className="content">
-            <div className="container-fluid">
-                <Session />
-                <SessionTable />
-            </div>
-        </section>
-      </div>
+    <div>
+      <Header/>
     </div>
   );
-};
+}
 
 export default App;
