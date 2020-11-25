b3013fd451fc1758f0d923d9edc2aa76d521dff6
Ramazan <ramazzan.ittiev@gmail.com>
2020-11-25 13:17:58 +0300

    Login component changes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,108 +1,118 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { create } from '../../Redux/Reducer/AuthReducer.js';
+import React, { Component, useEffect } from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import authThunk, { loginData } from '../../Redux/Auth/AuthAction.js';
+import loginReducer from '../../Redux/Auth/AuthReducer.js';
+import { LOGIN_DATA } from '../../Redux/Auth/AuthTypes.js';
 
-const Login = () => {
-  const dispatch = useDispatch();
-  const authStore = useSelector(state => state.authStore);
-
-  const onSubmit = e => {
-    e.preventDefault();
-    dispatch(create());
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
   };
 
-  return (
-    <div class="login-page">
-      <div className="login-box">
-        <div className="login-logo">
-          <a href="../../index2.html">
-            <b>Wellcome</b>
-          </a>
-        </div>
-        {/* /.login-logo */}
-        <div className="card">
-          <div className="card-body login-card-body">
-            <p className="login-box-msg">Sign in to start</p>
-            <form onSubmit={e => onSubmit(e)}>
-              <div className="input-group mb-3">
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control"
-                  placeholder="Email"
-                  // onChange={this.onChange}
-                />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-envelope" />
-                  </div>
-                </div>
-              </div>
-              <div className="input-group mb-3">
-                <input
-                  type="password"
-                  name="password"
-                  className="form-control"
-                  placeholder="Password"
-                  // onChange={this.onChange}
-                />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-lock" />
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                {/* /.col */}
-                <div className="col-4">
-                  <button type="submit" className="btn btn-primary btn-block">
-                    Log In
-                  </button>
-                </div>
-                {/* /.col */}
-              </div>
-            </form>
-            <p className="mb-0">
-              <NavLink to="/register" className="text-center">
-                Register a new membership
-              </NavLink>
-            </p>
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.loginReducer(this.state);
+  };
+
+  render() {
+    return (
+      <div class="login-page">
+        <div className="login-box">
+          <div className="login-logo">
+            <a href="../../index2.html">
+              <b>Wellcome</b>
+            </a>
           </div>
-          {/* /.login-card-body */}
+          {/* /.login-logo */}
+          <div className="card">
+            <div className="card-body login-card-body">
+              <p className="login-box-msg">Sign in to start</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="input-group mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    onChange={e => this.setState({ email: e.target.value })}
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-envelope" />
+                    </div>
+                  </div>
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    onChange={e => this.setState({ password: e.target.value })}
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-lock" />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  {/* /.col */}
+                  <div className="col-4">
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block"
+                      disabled={this.props.isLoading}
+                    >
+                      Log In
+                    </button>
+                  </div>
+                  {/* /.col */}
+                </div>
+              </form>
+              <p className="mb-0">
+                <NavLink to="/register" className="text-center">
+                  Register a new membership
+                </NavLink>
+              </p>
+            </div>
+            {/* /.login-card-body */}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Login;
-
-// handleSubmit = e => {
-//   e.preventDefault();
-
-//   const data = {
-//     email: this.email,
-//     password: this.password,
+// const mapStateToProps = state => {
+//   return {
+//     isLoading: state.isLoading,
+//     email: state.email,
+//     password: state.password,
+//     error: state.error,
 //   };
-
-//   Axios.post('auth', data)
-//     .then(res => {
-//       localStorage.setItem('token', res.data.token);
-
-//       this.setState({ auth: true });
-//     })
-//     .catch(err => {
-//       console.log('error');
-//     });
 // };
 
-// onChange = e => {
-//   this.setState({
-//     [e.target.name]: e.target.value,
-//   });
-// };
+// const mapDispatchToProps = dispatch => {
+//   return {
 
-// if (this.state.auth) {
-//   return <Redirect to="/home" />;
-// }
+//     // authThunk: () => dispatch(authThunk()),
+//   };
+// };
+export default connect(
+  state => ({
+    isLoading: state.isLoading,
+    email: state.email,
+    password: state.password,
+    error: state.error,
+  }),
+  dispatch => ({
+    loginReducer: (email, password) => {
+      const payload = {
+        email,
+        password,
+      };
+      dispatch({ type: LOGIN_DATA, payload });
+    },
+  }),
+)(Login);
